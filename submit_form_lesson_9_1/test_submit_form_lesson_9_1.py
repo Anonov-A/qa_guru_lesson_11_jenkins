d3e1dd6548c71db0c8ff04e9329b1fdae64e9348
aleksey antonov <antonovaaa174@gmail.com>
2026-01-03 01:17:28 +0500

    add_homework_to_lesson_11

---
+++ submit_form_lesson_9_1/test_submit_form_lesson_9_1.py
@@ -0,0 +1,45 @@
+import pytest
+import os
+import tempfile
+from selene import have
+from pages.registration_page import RegistrationPage
+
+
+
+
+
+def test_student_registration_form(setup_browser):
+    registration_page = RegistrationPage()
+
+    registration_page.open()
+    registration_page.fill_first_name('Алексей')
+    registration_page.fill_last_name('Антонов')
+    registration_page.fill_email('antonov@example.com')
+    registration_page.fill_gender('Male')
+    registration_page.fill_mobile_number('7951777777')
+    registration_page.fill_date('27 July,1900')
+    registration_page.select_subjects('Maths')
+    registration_page.select_hobbies('Music')
+
+    uploaded_filename = registration_page.load_picture()
+
+    registration_page.fill_adress('Puskina, Kolotuskina, 123456789')
+    registration_page.choice_state('NCR')
+    registration_page.choice_city('Delhi')
+    registration_page.click_submit()
+
+    registration_page.modal_window.should(have.text('Thanks for submitting the form'))
+
+    registration_page.info_about_the_registered_user.should(have.exact_texts(
+        'Алексей Антонов',
+        'antonov@example.com',
+        'Male',
+        '7951777777',
+        '27 July,1900',
+        'Maths',
+        'Music',
+        uploaded_filename,
+        'Puskina, Kolotuskina, 123456789',
+        'NCR Delhi'
+    ))
+
---
+++ submit_form_lesson_9_2/test_submit_form_lesson_9_2.py
@@ -0,0 +1,40 @@
+import pytest
+from datetime import date
+from selene import browser
+from selenium import webdriver
+from submit_form_lesson_9_2.models.models import User
+from submit_form_lesson_9_2.pages.pages import RegistrationPage
+
+
+@pytest.fixture(scope='function')
+def browser_management():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.driver_options = webdriver.ChromeOptions()
+    browser.config.driver_options.add_argument('--window-size=1920,1080')
+    browser.config.timeout = 30
+
+    yield
+
+
+
+
+def test_registration_alexey(browser_management):
+
+    user = User(
+        first_name='Алексей',
+        last_name='Антонов',
+        email='antonov@example.com',
+        gender='Male',
+        phone='7951777777',
+        birth_date=date(1900, 7, 27),
+        subjects=['Maths'],
+        hobbies=['Music'],
+        address='Puskina, Kolotuskina, 123456789',
+        state='NCR',
+        city='Delhi'
+    )
+
+    registration_page = RegistrationPage()
+
+    picture_filename = registration_page.open().register(user)
+    registration_page.should_have_registered(user, picture_filename)
